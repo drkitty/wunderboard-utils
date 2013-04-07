c415c539bf166810f7f1d69a8801af2ad866ab62
drkitty <bumblingkitty@gmail.com>
2013-04-06 17:56:33 -0700

    Change "row" to "col"
    Obviously we're dealing with columns, not rows.

--- characters.h
+++ characters.h
@@ -3,8 +3,8 @@
 
 #define INIT_LETTER_A() \
 struct character letter_A;\
-letter_A.rowCount = 6;\
-uint8_t letter_A_rows[6] = {\
+letter_A.colCount = 6;\
+uint8_t letter_A_cols[6] = {\
 	0b01111111,\
 	0b11111111,\
 	0b11001100,\
@@ -12,12 +12,12 @@ uint8_t letter_A_rows[6] = {\
 	0b11111111,\
 	0b01111111\
 };\
-letter_A.rows = letter_A_rows;
+letter_A.cols = letter_A_cols;
 
 #define INIT_LETTER_B() \
 struct character letter_B;\
-letter_B.rowCount = 6;\
-uint8_t letter_B_rows[6] = {\
+letter_B.colCount = 6;\
+uint8_t letter_B_cols[6] = {\
 	0b00000110,\
 	0b01101111,\
 	0b11111011,\
@@ -25,12 +25,12 @@ uint8_t letter_B_rows[6] = {\
 	0b11111111,\
 	0b11111111\
 };\
-letter_B.rows = letter_B_rows;
+letter_B.cols = letter_B_cols;
 
 #define INIT_LETTER_C() \
 struct character letter_C;\
-letter_C.rowCount = 6;\
-uint8_t letter_C_rows[6] = {\
+letter_C.colCount = 6;\
+uint8_t letter_C_cols[6] = {\
 	0b01100110,\
 	0b11000011,\
 	0b11000011,\
@@ -38,12 +38,12 @@ uint8_t letter_C_rows[6] = {\
 	0b11111111,\
 	0b01111110\
 };\
-letter_C.rows = letter_C_rows;
+letter_C.cols = letter_C_cols;
 
 #define INIT_LETTER_D() \
 struct character letter_D;\
-letter_D.rowCount = 6;\
-uint8_t letter_D_rows[6] = {\
+letter_D.colCount = 6;\
+uint8_t letter_D_cols[6] = {\
 	0b01111110,\
 	0b11111111,\
 	0b11000011,\
@@ -51,14 +51,14 @@ uint8_t letter_D_rows[6] = {\
 	0b11111111,\
 	0b11111111\
 };\
-letter_D.rows = letter_D_rows;
+letter_D.cols = letter_D_cols;
 
 /*
 
 #define INIT_LETTER_E() \
 struct character letter_E;\
-letter_E.rowCount = @;\
-uint8_t letter_E_rows[@] = {\
+letter_E.colCount = @;\
+uint8_t letter_E_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -66,12 +66,12 @@ uint8_t letter_E_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_E.rows = letter_E_rows;
+letter_E.cols = letter_E_cols;
 
 #define INIT_LETTER_F() \
 struct character letter_F;\
-letter_F.rowCount = @;\
-uint8_t letter_F_rows[@] = {\
+letter_F.colCount = @;\
+uint8_t letter_F_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -79,12 +79,12 @@ uint8_t letter_F_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_F.rows = letter_F_rows;
+letter_F.cols = letter_F_cols;
 
 #define INIT_LETTER_G() \
 struct character letter_G;\
-letter_G.rowCount = @;\
-uint8_t letter_G_rows[@] = {\
+letter_G.colCount = @;\
+uint8_t letter_G_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -92,14 +92,14 @@ uint8_t letter_G_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_G.rows = letter_G_rows;
+letter_G.cols = letter_G_cols;
 
 */
 
 #define INIT_LETTER_H() \
 struct character letter_H;\
-letter_H.rowCount = 6;\
-uint8_t letter_H_rows[6] = {\
+letter_H.colCount = 6;\
+uint8_t letter_H_cols[6] = {\
 	0b11111111,\
 	0b11111111,\
 	0b00011000,\
@@ -107,14 +107,14 @@ uint8_t letter_H_rows[6] = {\
 	0b11111111,\
 	0b11111111\
 };\
-letter_H.rows = letter_H_rows;
+letter_H.cols = letter_H_cols;
 
 /*
 
 #define INIT_LETTER_I() \
 struct character letter_I;\
-letter_I.rowCount = @;\
-uint8_t letter_I_rows[@] = {\
+letter_I.colCount = @;\
+uint8_t letter_I_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -122,12 +122,12 @@ uint8_t letter_I_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_I.rows = letter_I_rows;
+letter_I.cols = letter_I_cols;
 
 #define INIT_LETTER_J() \
 struct character letter_J;\
-letter_J.rowCount = @;\
-uint8_t letter_J_rows[@] = {\
+letter_J.colCount = @;\
+uint8_t letter_J_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -135,12 +135,12 @@ uint8_t letter_J_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_J.rows = letter_J_rows;
+letter_J.cols = letter_J_cols;
 
 #define INIT_LETTER_K() \
 struct character letter_K;\
-letter_K.rowCount = @;\
-uint8_t letter_K_rows[@] = {\
+letter_K.colCount = @;\
+uint8_t letter_K_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -148,12 +148,12 @@ uint8_t letter_K_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_K.rows = letter_K_rows;
+letter_K.cols = letter_K_cols;
 
 #define INIT_LETTER_L() \
 struct character letter_L;\
-letter_L.rowCount = @;\
-uint8_t letter_L_rows[@] = {\
+letter_L.colCount = @;\
+uint8_t letter_L_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -161,12 +161,12 @@ uint8_t letter_L_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_L.rows = letter_L_rows;
+letter_L.cols = letter_L_cols;
 
 #define INIT_LETTER_M() \
 struct character letter_M;\
-letter_M.rowCount = @;\
-uint8_t letter_M_rows[@] = {\
+letter_M.colCount = @;\
+uint8_t letter_M_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -174,12 +174,12 @@ uint8_t letter_M_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_M.rows = letter_M_rows;
+letter_M.cols = letter_M_cols;
 
 #define INIT_LETTER_N() \
 struct character letter_N;\
-letter_N.rowCount = @;\
-uint8_t letter_N_rows[@] = {\
+letter_N.colCount = @;\
+uint8_t letter_N_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -187,12 +187,12 @@ uint8_t letter_N_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_N.rows = letter_N_rows;
+letter_N.cols = letter_N_cols;
 
 #define INIT_LETTER_O() \
 struct character letter_O;\
-letter_O.rowCount = @;\
-uint8_t letter_O_rows[@] = {\
+letter_O.colCount = @;\
+uint8_t letter_O_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -200,12 +200,12 @@ uint8_t letter_O_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_O.rows = letter_O_rows;
+letter_O.cols = letter_O_cols;
 
 #define INIT_LETTER_P() \
 struct character letter_P;\
-letter_P.rowCount = @;\
-uint8_t letter_P_rows[@] = {\
+letter_P.colCount = @;\
+uint8_t letter_P_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -213,12 +213,12 @@ uint8_t letter_P_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_P.rows = letter_P_rows;
+letter_P.cols = letter_P_cols;
 
 #define INIT_LETTER_Q() \
 struct character letter_Q;\
-letter_Q.rowCount = @;\
-uint8_t letter_Q_rows[@] = {\
+letter_Q.colCount = @;\
+uint8_t letter_Q_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -226,12 +226,12 @@ uint8_t letter_Q_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_Q.rows = letter_Q_rows;
+letter_Q.cols = letter_Q_cols;
 
 #define INIT_LETTER_R() \
 struct character letter_R;\
-letter_R.rowCount = @;\
-uint8_t letter_R_rows[@] = {\
+letter_R.colCount = @;\
+uint8_t letter_R_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -239,12 +239,12 @@ uint8_t letter_R_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_R.rows = letter_R_rows;
+letter_R.cols = letter_R_cols;
 
 #define INIT_LETTER_S() \
 struct character letter_S;\
-letter_S.rowCount = @;\
-uint8_t letter_S_rows[@] = {\
+letter_S.colCount = @;\
+uint8_t letter_S_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -252,12 +252,12 @@ uint8_t letter_S_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_S.rows = letter_S_rows;
+letter_S.cols = letter_S_cols;
 
 #define INIT_LETTER_T() \
 struct character letter_T;\
-letter_T.rowCount = @;\
-uint8_t letter_T_rows[@] = {\
+letter_T.colCount = @;\
+uint8_t letter_T_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -265,12 +265,12 @@ uint8_t letter_T_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_T.rows = letter_T_rows;
+letter_T.cols = letter_T_cols;
 
 #define INIT_LETTER_U() \
 struct character letter_U;\
-letter_U.rowCount = @;\
-uint8_t letter_U_rows[@] = {\
+letter_U.colCount = @;\
+uint8_t letter_U_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -278,12 +278,12 @@ uint8_t letter_U_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_U.rows = letter_U_rows;
+letter_U.cols = letter_U_cols;
 
 #define INIT_LETTER_V() \
 struct character letter_V;\
-letter_V.rowCount = @;\
-uint8_t letter_V_rows[@] = {\
+letter_V.colCount = @;\
+uint8_t letter_V_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -291,12 +291,12 @@ uint8_t letter_V_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_V.rows = letter_V_rows;
+letter_V.cols = letter_V_cols;
 
 #define INIT_LETTER_W() \
 struct character letter_W;\
-letter_W.rowCount = @;\
-uint8_t letter_W_rows[@] = {\
+letter_W.colCount = @;\
+uint8_t letter_W_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -304,12 +304,12 @@ uint8_t letter_W_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_W.rows = letter_W_rows;
+letter_W.cols = letter_W_cols;
 
 #define INIT_LETTER_X() \
 struct character letter_X;\
-letter_X.rowCount = @;\
-uint8_t letter_X_rows[@] = {\
+letter_X.colCount = @;\
+uint8_t letter_X_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -317,12 +317,12 @@ uint8_t letter_X_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_X.rows = letter_X_rows;
+letter_X.cols = letter_X_cols;
 
 #define INIT_LETTER_Y() \
 struct character letter_Y;\
-letter_Y.rowCount = @;\
-uint8_t letter_Y_rows[@] = {\
+letter_Y.colCount = @;\
+uint8_t letter_Y_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -330,12 +330,12 @@ uint8_t letter_Y_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_Y.rows = letter_Y_rows;
+letter_Y.cols = letter_Y_cols;
 
 #define INIT_LETTER_Z() \
 struct character letter_Z;\
-letter_Z.rowCount = @;\
-uint8_t letter_Z_rows[@] = {\
+letter_Z.colCount = @;\
+uint8_t letter_Z_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -343,12 +343,12 @@ uint8_t letter_Z_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_Z.rows = letter_Z_rows;
+letter_Z.cols = letter_Z_cols;
 
 #define INIT_LETTER_a() \
 struct character letter_a;\
-letter_a.rowCount = @;\
-uint8_t letter_a_rows[@] = {\
+letter_a.colCount = @;\
+uint8_t letter_a_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -356,12 +356,12 @@ uint8_t letter_a_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_a.rows = letter_a_rows;
+letter_a.cols = letter_a_cols;
 
 #define INIT_LETTER_b() \
 struct character letter_b;\
-letter_b.rowCount = @;\
-uint8_t letter_b_rows[@] = {\
+letter_b.colCount = @;\
+uint8_t letter_b_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -369,12 +369,12 @@ uint8_t letter_b_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_b.rows = letter_b_rows;
+letter_b.cols = letter_b_cols;
 
 #define INIT_LETTER_c() \
 struct character letter_c;\
-letter_c.rowCount = @;\
-uint8_t letter_c_rows[@] = {\
+letter_c.colCount = @;\
+uint8_t letter_c_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -382,12 +382,12 @@ uint8_t letter_c_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_c.rows = letter_c_rows;
+letter_c.cols = letter_c_cols;
 
 #define INIT_LETTER_d() \
 struct character letter_d;\
-letter_d.rowCount = @;\
-uint8_t letter_d_rows[@] = {\
+letter_d.colCount = @;\
+uint8_t letter_d_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -395,28 +395,28 @@ uint8_t letter_d_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_d.rows = letter_d_rows;
+letter_d.cols = letter_d_cols;
 
 */
 
 #define INIT_LETTER_e() \
 struct character letter_e;\
-letter_e.rowCount = 5;\
-uint8_t letter_e_rows[5] = {\
+letter_e.colCount = 5;\
+uint8_t letter_e_cols[5] = {\
 	0b00110010,\
 	0b01111011,\
 	0b01101011,\
 	0b01111111,\
 	0b00111110\
 };\
-letter_e.rows = letter_e_rows;
+letter_e.cols = letter_e_cols;
 
 /*
 
 #define INIT_LETTER_f() \
 struct character letter_f;\
-letter_f.rowCount = @;\
-uint8_t letter_f_rows[@] = {\
+letter_f.colCount = @;\
+uint8_t letter_f_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -424,12 +424,12 @@ uint8_t letter_f_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_f.rows = letter_f_rows;
+letter_f.cols = letter_f_cols;
 
 #define INIT_LETTER_g() \
 struct character letter_g;\
-letter_g.rowCount = @;\
-uint8_t letter_g_rows[@] = {\
+letter_g.colCount = @;\
+uint8_t letter_g_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -437,12 +437,12 @@ uint8_t letter_g_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_g.rows = letter_g_rows;
+letter_g.cols = letter_g_cols;
 
 #define INIT_LETTER_h() \
 struct character letter_h;\
-letter_h.rowCount = @;\
-uint8_t letter_h_rows[@] = {\
+letter_h.colCount = @;\
+uint8_t letter_h_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -450,12 +450,12 @@ uint8_t letter_h_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_h.rows = letter_h_rows;
+letter_h.cols = letter_h_cols;
 
 #define INIT_LETTER_i() \
 struct character letter_i;\
-letter_i.rowCount = @;\
-uint8_t letter_i_rows[@] = {\
+letter_i.colCount = @;\
+uint8_t letter_i_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -463,12 +463,12 @@ uint8_t letter_i_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_i.rows = letter_i_rows;
+letter_i.cols = letter_i_cols;
 
 #define INIT_LETTER_j() \
 struct character letter_j;\
-letter_j.rowCount = @;\
-uint8_t letter_j_rows[@] = {\
+letter_j.colCount = @;\
+uint8_t letter_j_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -476,12 +476,12 @@ uint8_t letter_j_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_j.rows = letter_j_rows;
+letter_j.cols = letter_j_cols;
 
 #define INIT_LETTER_k() \
 struct character letter_k;\
-letter_k.rowCount = @;\
-uint8_t letter_k_rows[@] = {\
+letter_k.colCount = @;\
+uint8_t letter_k_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -489,26 +489,26 @@ uint8_t letter_k_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_k.rows = letter_k_rows;
+letter_k.cols = letter_k_cols;
 
 */
 
 #define INIT_LETTER_l() \
 struct character letter_l;\
-letter_l.rowCount = 3;\
-uint8_t letter_l_rows[3] = {\
+letter_l.colCount = 3;\
+uint8_t letter_l_cols[3] = {\
 	0b00000011,\
 	0b11111111,\
 	0b11111100\
 };\
-letter_l.rows = letter_l_rows;
+letter_l.cols = letter_l_cols;
 
 /*
 
 #define INIT_LETTER_m() \
 struct character letter_m;\
-letter_m.rowCount = @;\
-uint8_t letter_m_rows[@] = {\
+letter_m.colCount = @;\
+uint8_t letter_m_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -516,12 +516,12 @@ uint8_t letter_m_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_m.rows = letter_m_rows;
+letter_m.cols = letter_m_cols;
 
 #define INIT_LETTER_n() \
 struct character letter_n;\
-letter_n.rowCount = @;\
-uint8_t letter_n_rows[@] = {\
+letter_n.colCount = @;\
+uint8_t letter_n_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -529,28 +529,28 @@ uint8_t letter_n_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_n.rows = letter_n_rows;
+letter_n.cols = letter_n_cols;
 
 */
 
 #define INIT_LETTER_o() \
 struct character letter_o;\
-letter_o.rowCount = 5;\
-uint8_t letter_o_rows[5] = {\
+letter_o.colCount = 5;\
+uint8_t letter_o_cols[5] = {\
 	0b00011110,\
 	0b00111111,\
 	0b00110011,\
 	0b00111111,\
 	0b00011110\
 };\
-letter_o.rows = letter_o_rows;
+letter_o.cols = letter_o_cols;
 
 /*
 
 #define INIT_LETTER_p() \
 struct character letter_p;\
-letter_p.rowCount = @;\
-uint8_t letter_p_rows[@] = {\
+letter_p.colCount = @;\
+uint8_t letter_p_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -558,12 +558,12 @@ uint8_t letter_p_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_p.rows = letter_p_rows;
+letter_p.cols = letter_p_cols;
 
 #define INIT_LETTER_q() \
 struct character letter_q;\
-letter_q.rowCount = @;\
-uint8_t letter_q_rows[@] = {\
+letter_q.colCount = @;\
+uint8_t letter_q_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -571,12 +571,12 @@ uint8_t letter_q_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_q.rows = letter_q_rows;
+letter_q.cols = letter_q_cols;
 
 #define INIT_LETTER_r() \
 struct character letter_r;\
-letter_r.rowCount = @;\
-uint8_t letter_r_rows[@] = {\
+letter_r.colCount = @;\
+uint8_t letter_r_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -584,12 +584,12 @@ uint8_t letter_r_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_r.rows = letter_r_rows;
+letter_r.cols = letter_r_cols;
 
 #define INIT_LETTER_s() \
 struct character letter_s;\
-letter_s.rowCount = @;\
-uint8_t letter_s_rows[@] = {\
+letter_s.colCount = @;\
+uint8_t letter_s_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -597,12 +597,12 @@ uint8_t letter_s_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_s.rows = letter_s_rows;
+letter_s.cols = letter_s_cols;
 
 #define INIT_LETTER_t() \
 struct character letter_t;\
-letter_t.rowCount = @;\
-uint8_t letter_t_rows[@] = {\
+letter_t.colCount = @;\
+uint8_t letter_t_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -610,12 +610,12 @@ uint8_t letter_t_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_t.rows = letter_t_rows;
+letter_t.cols = letter_t_cols;
 
 #define INIT_LETTER_u() \
 struct character letter_u;\
-letter_u.rowCount = @;\
-uint8_t letter_u_rows[@] = {\
+letter_u.colCount = @;\
+uint8_t letter_u_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -623,12 +623,12 @@ uint8_t letter_u_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_u.rows = letter_u_rows;
+letter_u.cols = letter_u_cols;
 
 #define INIT_LETTER_v() \
 struct character letter_v;\
-letter_v.rowCount = @;\
-uint8_t letter_v_rows[@] = {\
+letter_v.colCount = @;\
+uint8_t letter_v_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -636,12 +636,12 @@ uint8_t letter_v_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_v.rows = letter_v_rows;
+letter_v.cols = letter_v_cols;
 
 #define INIT_LETTER_w() \
 struct character letter_w;\
-letter_w.rowCount = @;\
-uint8_t letter_w_rows[@] = {\
+letter_w.colCount = @;\
+uint8_t letter_w_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -649,12 +649,12 @@ uint8_t letter_w_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_w.rows = letter_w_rows;
+letter_w.cols = letter_w_cols;
 
 #define INIT_LETTER_x() \
 struct character letter_x;\
-letter_x.rowCount = @;\
-uint8_t letter_x_rows[@] = {\
+letter_x.colCount = @;\
+uint8_t letter_x_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -662,12 +662,12 @@ uint8_t letter_x_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_x.rows = letter_x_rows;
+letter_x.cols = letter_x_cols;
 
 #define INIT_LETTER_y() \
 struct character letter_y;\
-letter_y.rowCount = @;\
-uint8_t letter_y_rows[@] = {\
+letter_y.colCount = @;\
+uint8_t letter_y_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -675,12 +675,12 @@ uint8_t letter_y_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_y.rows = letter_y_rows;
+letter_y.cols = letter_y_cols;
 
 #define INIT_LETTER_z() \
 struct character letter_z;\
-letter_z.rowCount = @;\
-uint8_t letter_z_rows[@] = {\
+letter_z.colCount = @;\
+uint8_t letter_z_cols[@] = {\
 	0b00000000,\
 	0b00000000,\
 	0b00000000,\
@@ -688,17 +688,17 @@ uint8_t letter_z_rows[@] = {\
 	0b00000000,\
 	0b00000000\
 };\
-letter_z.rows = letter_z_rows;
+letter_z.cols = letter_z_cols;
 
 */
 
 #define INIT_EXCLAMATION_POINT() \
 struct character exclamationPoint;\
-exclamationPoint.rowCount = 2;\
-uint8_t exclamationPoint_rows[2] = {\
+exclamationPoint.colCount = 2;\
+uint8_t exclamationPoint_cols[2] = {\
 	0b11111011,\
 	0b11111011\
 };\
-exclamationPoint.rows = exclamationPoint_rows;
+exclamationPoint.cols = exclamationPoint_cols;
 
 #endif // CHARACTERS_H
