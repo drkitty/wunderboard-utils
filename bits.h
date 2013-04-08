1408cbc5411c4b02a8b925d3df95805be9c58471
drkitty <bumblingkitty@gmail.com>
2013-04-07 17:35:32 -0700

    Semicolons are helpful

--- bits.h
+++ bits.h
@@ -4,7 +4,7 @@
 // uint8_t:
 #include <avr/io.h>
 
-uint8_t replaceBit(uint8_t base, uint8_t newBit, uint8_t place)
+uint8_t replaceBit(uint8_t base, uint8_t newBit, uint8_t place);
 
 // mask: 1 = replace this bit
 uint8_t replaceBits_mask(uint8_t base, uint8_t newBits, uint8_t mask);
