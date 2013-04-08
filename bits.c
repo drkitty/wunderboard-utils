4391cead0df06eca90d03f79effdb43791786f31
drkitty <bumblingkitty@gmail.com>
2013-04-07 18:08:14 -0700

    Add getBit. Also, K&R are right.

--- bits.c
+++ bits.c
@@ -1,5 +1,10 @@
 #include "bits.h"
 
+uint8_t getBit(uint8_t byte, uint8_t place)
+{
+	return (byte & (1<<place)) ? 1 : 0;
+}
+
 uint8_t replaceBit(uint8_t base, uint8_t newBit, uint8_t place)
 {
 	if (newBit)
@@ -8,13 +13,15 @@ uint8_t replaceBit(uint8_t base, uint8_t newBit, uint8_t place)
 		return base & (0xFF ^ (1 << place));
 }
 
-uint8_t replaceBits_mask(uint8_t base, uint8_t newBits, uint8_t mask) {
+uint8_t replaceBits_mask(uint8_t base, uint8_t newBits, uint8_t mask)
+{
 	return base ^ ((base ^ newBits) & mask);
 	//     base ^ ((     delta    ) & mask)
 	//     base ^ (     masked delta      )
 }
 
-void explodeByte(uint8_t bits, uint8_t array[8]) {
+void explodeByte(uint8_t bits, uint8_t array[8])
+{
 		for (uint8_t i=0; i <= 7; i++) {
 			array[i] = (bits & (1<<i)) ? 1 : 0;
 		};
--- bits.h
+++ bits.h
@@ -4,6 +4,8 @@
 // uint8_t:
 #include <avr/io.h>
 
+inline uint8_t getBit(uint8_t byte, uint8_t place);
+
 uint8_t replaceBit(uint8_t base, uint8_t newBit, uint8_t place);
 
 // mask: 1 = replace this bit
