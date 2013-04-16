62fd9827c12c847268418a187d5bc8649ca0ec5e
drkitty <bumblingkitty@gmail.com>
2013-04-15 18:00:36 -0700

    Manually unroll color loop

--- screen.c
+++ screen.c
@@ -35,15 +35,28 @@ void screenArray(const uint8_t red[8], const uint8_t green[8],
 void screenArrayFullColor(uint8_t red[64], uint8_t green[64], uint8_t blue[64],
 		uint8_t* counter, uint8_t brightnessLevels)
 {
+	/* red, green, blue */
 	uint8_t colBits[3];
-	uint8_t* bufferSelector[3] = {red, green, blue};
-	for (uint8_t col = 0; col <= 7; col++) {
-		for (uint8_t color = 0; color <= 2; color++) {
-			colBits[color] = 0x00;
-			for (uint8_t row = 0; row <= 7; row++) {
-				if (bufferSelector[color][(7-row)*8 + (7-col)] > *counter)
-					colBits[color] |= 1<<row;
-			};
+
+	uint8_t col, row;
+
+	for (col = 0; col <= 7; col++) {
+		colBits[0] = 0x00;
+		for (row = 0; row <= 7; row++) {
+			if (red[(7-row)*8 + (7-col)] > *counter)
+				colBits[0] |= 1<<row;
+		};
+
+		colBits[1] = 0x00;
+		for (row = 0; row <= 7; row++) {
+			if (green[(7-row)*8 + (7-col)] > *counter)
+				colBits[1] |= 1<<row;
+		};
+
+		colBits[2] = 0x00;
+		for (row = 0; row <= 7; row++) {
+			if (blue[(7-row)*8 + (7-col)] > *counter)
+				colBits[2] |= 1<<row;
 		};
 		screenCol(col, colBits[0], colBits[1], colBits[2]);
 		screenCol(col, 0x00, 0x00, 0x00);
