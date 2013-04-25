162602771d72962db16ebb77a6fba20bcc178b89
drkitty <bumblingkitty@gmail.com>
2013-04-24 17:02:51 -0700

    Move freq search and add another freq util

---
+++ tools/T0_bestfreq.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from T0_COMPA_frequency import *
+
+print("N   OCR0A   freq")
+def main():
+	for N in [1, 8, 256, 1024]: # prescaler
+		minFreqError = 1
+		bestFreq = int()
+		bestOCR0A = int()
+		for OCR0A in range(0xFF):
+			freq = f_COMPA(N, OCR0A) # frequency of COMPA interrupt
+			freqError = abs(freq - int(freq))
+			if (freqError < minFreqError):
+				minFreqError = freqError
+				bestFreq = freq
+				bestOCR0A = OCR0A
+			#print("N = {}, OCR0A = 0x{:X}".format(N, OCR0A))
+			#print("freq = {}".format(freq))
+		print("{}   0x{:X}   {}".format(N, bestOCR0A,bestFreq))
+		if N != 1024:
+			print()
+
+if __name__ == "__main__":
+	main()
---
+++ tools/T0_freqlist.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from T0_COMPA_frequency import *
+from sys import argv
+
+try:
+	maxFreqError = float(argv[1])
+except:
+	print("ERROR: argument error")
+	print()
+	print("usage:")
+	print("\tT0_COMPA_frequency_list.py maxFrequencyError")
+	exit()
+
+
+print("N   OCR0A   freq")
+def main():
+	for N in {1, 8, 256, 1024}: # prescaler
+		for OCR0A in range(0xFF):
+			freq = f_COMPA(N, OCR0A) # frequency of COMPA interrupt
+			freqError = abs(freq - int(freq))
+			if freqError < maxFreqError:
+				print("{}   0x{:X}   {}".format(N, OCR0A, freq))
+				#print("{}\t0x{:X}\t{}".format(N, OCR0A, freq))
+
+if __name__ == "__main__":
+	main()
